9de616f7eef158fe32a707c4fdd8cadaf16ac4b7
Rahul Popuri <rpopuri@tripactions.com>
2020-12-08 22:21:03 -0800

    day 9

---
+++ src/main/kotlin/aoc/day9/Day9.kt
@@ -0,0 +1,55 @@
+package aoc.day9
+
+import java.io.File
+import java.io.InputStream
+
+fun main() {
+    val input = loadInput("input_day9.txt")
+    println(firstInvalidNum(input, 25))
+    println(contiguousSum(input, 393911906))
+}
+
+fun firstInvalidNum(input: List<Long>, preamble: Int): Long {
+    for (i in preamble until input.size - 1) {
+        val subList = input.subList(i - preamble, i)
+        if (!twoSumExists(subList, input[i])) {
+            return input[i]
+        }
+    }
+    return -1
+}
+
+fun twoSumExists(a: List<Long>, target: Long): Boolean {
+    for (i in a) {
+        if (a.contains(target - i)) return true
+    }
+    return false
+}
+
+fun contiguousSum(a: List<Long>, target: Long): Long {
+    val sum = LongArray(a.size + 1)
+    sum[0] = 0
+    for (i in 1 until a.size) {
+        sum[i] = sum[i - 1] + a[i - 1]
+    }
+    for (start in 0 until a.size - 1) {
+        for (end in start + 1 until a.size) {
+            if (sum[end] - sum[start] == target) {
+                val subList = a.subList(start + 1, end + 1).sorted()
+                return subList[0] + subList[subList.size - 1]
+            }
+        }
+    }
+    return 0
+}
+
+fun loadInput(inputFile: String): List<Long> {
+    val inputStream: InputStream = File(ClassLoader.getSystemResource(inputFile).file).inputStream()
+    val result = mutableListOf<Long>()
+    inputStream.bufferedReader().forEachLine { line ->
+        run {
+            result.add(line.toLong())
+        }
+    }
+    return result
+}
